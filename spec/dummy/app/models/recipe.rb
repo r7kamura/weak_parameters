cee6b706d2a145a15ebca6a1c28fca41b500ea7a
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-08-13 16:24:13 +0900

    Transit to Rails 4

--- spec/dummy/app/models/recipe.rb
+++ spec/dummy/app/models/recipe.rb
@@ -1,3 +1,2 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :name, :type
 end
--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '2bc71f32e8d9d76cbdf4c01f3e959bb186b372ae67429c06fc88d82073e7e54170ca2b6e92f5487cb0dfaba18783a5f266cb5d50d13fee05021f46049b33fa0c'
+Dummy::Application.config.secret_key_base = '24b191b073b60770ff32d8c0096a6786c8aa0c5f7c0968838f5321a0fbda49da3a567984ca3862f353b4cc408e1e23fedd1f3d808ae91965758239f19642a2e6'
