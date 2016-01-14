d5a8a8893f805660ba93a6ee71aad2bbdd5ce646
r7kamura <r7kamura@gmail.com>
2016-01-14 13:15:34 +0900

    Remove deperecated code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../spec/dummy/config/environment", __FILE__)
 require "rspec/rails"
-require "rspec/autorun"
 
 RSpec.configure do |config|
   # If you"re not using ActiveRecord, or you"d prefer not to run each of your
@@ -13,6 +12,4 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
